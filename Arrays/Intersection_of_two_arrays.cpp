73bef6ab1a128cec8c5479c718d6e760b0306735
buwagaurav <gauravbuwa98@gmail.com>
2022-02-18 11:28:15 +0530

    update: comment

--- Arrays/Intersection_of_two_arrays.cpp
+++ Arrays/Intersection_of_two_arrays.cpp
@@ -1,5 +1,5 @@
 
-// Given two arrays a[] and b[] of size n and n respectively. 
+// Given two arrays a[] and b[] of size n and m respectively. 
 // The task is to find no. Intersection of two arrays.
 // O(n*n) solution is to use two loops and check every element by comparing from each array
 // using sorting it is O(nlog(n))
