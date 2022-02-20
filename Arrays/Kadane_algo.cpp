cceb6b5c7ce79fa21cd990fc8b8b6b3faa5661ab
buwagaurav <gauravbuwa98@gmail.com>
2022-02-20 19:55:49 +0530

    update

---
+++ Arrays/Kadane_algo.cpp
@@ -0,0 +1,26 @@
+/*
+//Given an array arr of N integers. Find the contiguous sub-array with maximum sum.
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int sum=0,n;
+    cin>>n;
+    vector<int> a;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int maxi = a[0];
+    for(auto it: a){
+        sum+= it;
+        maxi = max(maxi,sum);
+        if(sum<0)sum=0;
+    }
+    cout<<maxi;
+    
+    return 0;
+}
